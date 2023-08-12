d73aa89cb1c4e731bb3d39e86ce2a68f6b879e2b
dariasantana <cs.darias@gmail.com>
2023-08-12 16:25:10 +0100

    algorithm fixed

--- js/index.js
+++ js/index.js
@@ -3,28 +3,35 @@ const close_btn=document.querySelector('.close__icon');
 const nav_links=document.querySelectorAll('.nav__links');
 const nav_link=document.querySelectorAll('.nav__link');
 const mediaQuery=window.matchMedia('(max-width:768px)');
+const body=document.querySelector('body');
 
 
-if(mediaQuery.matches){
-    open_btn.addEventListener('click', ()=>{
-        open_btn.style.display='none';
-        close_btn.style.display='block';
-        nav_link.forEach(nav_link=> nav_link.classList.add('visible'));
-        nav_links.forEach(nav_links=>nav_links.classList.add('visible'));
-        
-        
-    })
+function navMenuFixes(){
+    if(mediaQuery.matches){
+        open_btn.addEventListener('click', ()=>{
+            open_btn.style.display='none';
+            close_btn.style.display='block';
+            body.style.overflowY = 'hidden';
+            nav_link.forEach(nav_link=> nav_link.classList.add('visible'));
+            nav_links.forEach(nav_links=>nav_links.classList.add('visible'));
 
-    close_btn.addEventListener('click', ()=>{
+        })
+
+        close_btn.addEventListener('click', ()=>{
+            open_btn.style.display='block';
+            close_btn.style.display='none';
+            nav_link.forEach(nav_link=> nav_link.classList.remove('visible'));
+            nav_links.forEach(nav_links=>nav_links.classList.remove('visible'));
+        })
+    }else {
         open_btn.style.display='block';
         close_btn.style.display='none';
-        nav_links.forEach(nav_link=> nav_link.classList.remove('visible'));
-        nav_links.forEach(nav_links=>nav_links.classList.remove('visible'));
-        
-    })
-
+        nav_link.forEach(nav_link => nav_link.classList.remove('visible'));
+        nav_links.forEach(nav_links => nav_links.classList.remove('visible'));
+    
+    }
 }
 
+window.addEventListener('resize', navMenuFixes);
 
-
-
+navMenuFixes();
